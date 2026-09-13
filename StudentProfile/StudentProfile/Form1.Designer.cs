d9ac615976cd7b6de5e8b0a2f4f044efdb602efe
jeanmargarettepasia591-boop <jeanmargarettepasia591@gmail.com>
2026-09-13 21:21:29 +0800

    Add student contact number

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -29,26 +29,37 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(94, 178);
+            this.label1.Location = new System.Drawing.Point(22, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(543, 38);
+            this.label1.Size = new System.Drawing.Size(264, 38);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Student Profile GitHub Beginner Lab";
+            this.label1.Text = "Contact Number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(306, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 45);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "09510066337";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 430);
+            this.ClientSize = new System.Drawing.Size(572, 305);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
